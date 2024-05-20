d1f2e5764643892f59b0225eb4b81ef554ad93df
Fernando Picasso <nando.picasso@gmail.com>
2024-05-20 15:51:03 +0200

    Removed tap-dances. Added altgr keys as combos and to NAV layer.

--- keyboards/keychron/q1_pro/iso_knob/keymaps/fernandopicasso/keymap.c
+++ keyboards/keychron/q1_pro/iso_knob/keymaps/fernandopicasso/keymap.c
@@ -35,10 +35,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NAV] = LAYOUT_iso_83(
         KC_ESC,          KC_F1,    KC_F2,      KC_F3,    KC_F4,    KC_F5,    KC_F6,      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_DEL,            KC_MUTE,
-        XXXXXXX,         XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_BSPC,           KC_PGUP,
-        KC_TAB,          XXXXXXX,  C(KC_RGHT), XXXXXXX,  XXXXXXX,  XXXXXXX,  C(KC_C),    C(KC_Z),  XXXXXXX,  XXXXXXX,  C(KC_V),  XXXXXXX,   XXXXXXX,  KC_ENT,            KC_PGDN,
+        XXXXXXX,         XXXXXXX,  FI_AT,      XXXXXXX,  FI_DLR,   XXXXXXX,  XXXXXXX,    FI_LCBR,  FI_LBRC,  FI_RBRC,  FI_RCBR,  FI_BSLS,   XXXXXXX,  KC_BSPC,           KC_PGUP,
+        KC_TAB,          XXXXXXX,  C(KC_RGHT), XXXXXXX,  XXXXXXX,  XXXXXXX,  C(KC_C),    C(KC_Z),  XXXXXXX,  XXXXXXX,  C(KC_V),  XXXXXXX,   FI_TILD,  KC_ENT,            KC_PGDN,
         KC_ESC,          XXXXXXX,  XXXXXXX,    C(KC_X),  XXXXXXX,  XXXXXXX,  KC_LEFT,    KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  XXXXXXX,             XXXXXXX,           KC_HOME,
-        KC_LSFT,         XXXXXXX,  XXXXXXX,    KC_DEL,   XXXXXXX,  XXXXXXX,  C(KC_LEFT), XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,             KC_RSFT,  KC_UP,
+        KC_LSFT,         FI_PIPE,  XXXXXXX,    KC_DEL,   XXXXXXX,  XXXXXXX,  C(KC_LEFT), XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,             KC_RSFT,  KC_UP,
         KC_LCTL,         KC_LGUI,  KC_LALT,                                  XXXXXXX,                                  KC_RALT,  MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_iso_83(
@@ -71,11 +71,29 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 const uint16_t PROGMEM ctxmenu_combo[] = {KC_RALT, KC_RCTL, COMBO_END};
 const uint16_t PROGMEM backsp_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM del_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM at_combo[] = {FI_1, FI_2, COMBO_END};
+const uint16_t PROGMEM dlr_combo[] = {FI_3, FI_4, COMBO_END};
+const uint16_t PROGMEM bsls_combo[] = {FI_PLUS, FI_ACUT, COMBO_END};
+const uint16_t PROGMEM tild_combo[] = {FI_ARNG, FI_DIAE, COMBO_END};
+const uint16_t PROGMEM pipe_combo[] = {FI_LABK, FI_Z, COMBO_END};
+const uint16_t PROGMEM lcbr_combo[] = {FI_6, FI_7, COMBO_END};
+const uint16_t PROGMEM lbrc_combo[] = {FI_7, FI_8, COMBO_END};
+const uint16_t PROGMEM rbrc_combo[] = {FI_9, FI_0, COMBO_END};
+const uint16_t PROGMEM rcbr_combo[] = {FI_0, FI_PLUS, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(ctxmenu_combo, KC_APP),
     COMBO(backsp_combo, KC_BSPC),
     COMBO(del_combo, KC_DEL),
+    COMBO(at_combo, FI_AT),
+    COMBO(dlr_combo, FI_DLR),
+    COMBO(bsls_combo, FI_BSLS),
+    COMBO(tild_combo, FI_TILD),
+    COMBO(pipe_combo, FI_PIPE),
+    COMBO(lcbr_combo, FI_LCBR),
+    COMBO(rcbr_combo, FI_RCBR),
+    COMBO(lbrc_combo, FI_LBRC),
+    COMBO(rbrc_combo, FI_RBRC),
 };
 
 uint16_t COMBO_LEN = sizeof(key_combos) / sizeof(*key_combos);
